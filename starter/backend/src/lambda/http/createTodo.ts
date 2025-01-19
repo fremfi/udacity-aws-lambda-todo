709eac6017fc55ef081ae992378fc195ebeaebfc
Freddie <frederick@outschool.com>
2025-01-19 14:37:57 -0500

    add validation

--- starter/backend/src/lambda/http/createTodo.ts
+++ starter/backend/src/lambda/http/createTodo.ts
@@ -7,6 +7,37 @@ import cors from '@middy/http-cors'
 import httpErrorHandler from '@middy/http-error-handler'
 import {getToken, getUserId} from "../../auth/jwt";
 import {APIGatewayProxyEvent} from "aws-lambda";
+import Ajv from 'ajv';
+
+const ajv = new Ajv();
+
+const createTodoSchema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "create-todo",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string",
+            "pattern": "^[a-zA-Z0-9]+$",
+            "minLength": 5,
+            "maxLength": 20
+        },
+        "dueDate": {
+            "type": "string",
+            "pattern": "[0-9]{4}-[0-9]{2}-[0-9]{2}"
+        }
+    },
+    "required": ["name", "dueDate"],
+    "additionalProperties": {
+        "properties": {
+            "attachmentUrl": {
+                "type": "string"
+            }
+        }
+    }
+};
+
+const validateTodo = ajv.compile(createTodoSchema);
 
 export const handler = middy()
     .use(httpErrorHandler())
@@ -16,19 +47,31 @@ export const handler = middy()
         })
     )
     .handler(async (event: APIGatewayProxyEvent) => {
-  const newTodoData: TodoCreate = JSON.parse(event.body);
+    const newTodoData: TodoCreate = JSON.parse(event.body);
+    const valid = validateTodo(newTodoData);
 
-  try {
+    if (!valid) {
+        // If validation fails, return an error response
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'Invalid input data',
+                details: validateTodo.errors,
+            })
+        };
+    }
+
+    try {
       const jwtToken: string = getToken(event);
       const newTodo: TodoItem = await createTodo(getUserId(jwtToken), newTodoData);
       return {
           statusCode: 201,
           body: JSON.stringify({ newTodo })
       };
-  } catch (error) {
+    } catch (error) {
       return {
           statusCode: 500,
           body: JSON.stringify({ error })
       };
-  }
+    }
 });
--- starter/backend/src/lambda/http/updateTodo.ts
+++ starter/backend/src/lambda/http/updateTodo.ts
@@ -6,6 +6,30 @@ import httpErrorHandler from '@middy/http-error-handler'
 import {getToken, getUserId} from "../../auth/jwt";
 import {APIGatewayProxyEvent} from "aws-lambda";
 import {TodoUpdate} from "../../models/TodoUpdate";
+import Ajv from 'ajv';
+
+const ajv = new Ajv();
+
+const updateTodoSchema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "update-todo",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string"
+        },
+        "dueDate": {
+            "type": "string"
+        },
+        "done": {
+            "type": "boolean"
+        }
+    },
+    "required": ["name", "dueDate", "done"],
+    "additionalProperties": false
+};
+
+const validateUpdateTodo = ajv.compile(updateTodoSchema);
 
 export const handler = middy()
     .use(httpErrorHandler())
@@ -15,20 +39,31 @@ export const handler = middy()
         })
     )
     .handler(async (event: APIGatewayProxyEvent) => {
-      const todoId = event.pathParameters.todoId;
-      const updateData: TodoUpdate = JSON.parse(event.body);
+        const todoId = event.pathParameters.todoId;
+        const updateData: TodoUpdate = JSON.parse(event.body);
+        const valid = validateUpdateTodo(updateData);
 
-      try {
-        const jwtToken: string = getToken(event);
-        await updateTodo(getUserId(jwtToken), todoId, updateData);
-        return {
-          statusCode: 204,
-            body: JSON.stringify({todoId})
-        };
-      } catch (error) {
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error })
-        };
-      }
+        if (!valid) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: 'Invalid input data',
+                    details: validateUpdateTodo.errors,
+                })
+            };
+        }
+
+        try {
+            const jwtToken: string = getToken(event);
+            await updateTodo(getUserId(jwtToken), todoId, updateData);
+            return {
+              statusCode: 204,
+                body: JSON.stringify({todoId})
+            };
+        } catch (error) {
+            return {
+              statusCode: 500,
+              body: JSON.stringify({ error })
+            };
+        }
     });
